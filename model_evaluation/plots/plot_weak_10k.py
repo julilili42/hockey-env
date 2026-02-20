e2a6dc4a90e0eb02c66fd9a210fee00f51e604f6
julilili42 <116183344+julilili42@users.noreply.github.com>
2026-02-20 14:26:36 +0100

    Refactored code

--- model_evaluation/plots/plot_weak_10k.py
+++ model_evaluation/plots/plot_weak_10k.py
@@ -1,9 +1,9 @@
 from rl.utils.plotter import MetricsPlotter
 
-json_path = "pretrained/weak_10k/metrics/metrics.json"
-save_dir = "model_evaluation/plots/outputs/final_12k"
+json_path = "pretrained/stage_1/metrics/metrics.json"
+save_dir = "model_evaluation/plots/outputs/final_12k1"
 
 plotter = MetricsPlotter.from_json(json_path)
-plotter.save_all(save_dir, show="weak")
+plotter.save_all(save_dir, show="both")
 
 print("Plots saved to:", save_dir)
